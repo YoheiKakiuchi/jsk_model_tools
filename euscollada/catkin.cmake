45b325bb49fc95eadbad6404af3afd2e8e1bc297
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2014-09-30 23:14:56 +0900

    update collada2eus

--- euscollada/catkin.cmake
+++ euscollada/catkin.cmake
@@ -15,9 +15,9 @@ pkg_check_modules(assimpdevel assimp_devel REQUIRED)
 include_directories(${catkin_INCLUDE_DIRS} ${colladadom_INCLUDE_DIRS} ${yaml_cpp_INCLUDE_DIRS} ${assimpdevel_INCLUDE_DIRS})
 link_directories(${catkin_LIBRARY_DIRS} ${assimpdevel_LIBRARY_DIRS})
 
-add_executable(collada2eus src/collada2eus.cpp)
-target_link_libraries(collada2eus ${catkin_LIBRARIES} qhull ${yaml_cpp_LIBRARIES} ${colladadom_LIBRARIES} ${recource_retriever_LIBRARIES})
-add_dependencies(collada2eus libassimp_devel)
+add_executable(collada2eus_old src/collada2eus.cpp)
+target_link_libraries(collada2eus_old ${catkin_LIBRARIES} qhull ${yaml_cpp_LIBRARIES} ${colladadom_LIBRARIES} ${recource_retriever_LIBRARIES})
+add_dependencies(collada2eus_old libassimp_devel)
 
 find_package(Boost REQUIRED system)
 include_directories(${Boost_INCLUDE_DIR})
@@ -25,7 +25,7 @@ add_executable(collada2eus_urdfmodel src/collada2eus_urdfmodel.cpp)
 target_link_libraries(collada2eus_urdfmodel ${catkin_LIBRARIES} qhull ${yaml_cpp_LIBRARIES} ${colladadom_LIBRARIES} ${collada_parser_LIBRARIES} ${recource_retriever_LIBRARIES} ${assimpdevel_LIBRARIES} ${Boost_LIBRARIES})
 add_dependencies(collada2eus_urdfmodel libassimp_devel)
 
-install(TARGETS collada2eus collada2eus_urdfmodel
+install(TARGETS collada2eus_old collada2eus_urdfmodel
         RUNTIME DESTINATION ${CATKIN_PACKAGE_BIN_DESTINATION}
         ARCHIVE DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
         LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})
